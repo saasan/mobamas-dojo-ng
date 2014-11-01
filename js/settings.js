333f60a199e247c008dd358d68cb757f5f89b445
saasan <saasan.s2works@hotmail.com>
2014-11-02 07:32:36 +0900

    設定画面をざっくり

---
+++ js/settings.js
@@ -0,0 +1,62 @@
+angular.module('dojoSettings', [])
+  .controller('SettingsController', ['$scope', function($scope) {
+    $scope.settings = {
+      otherTab: true,
+      visitedMax: 1,
+      autoHide: true,
+      keepLastVisited: true,
+      numberOfShownDojos: 20,
+      maxDefence: 0,
+      showBirthday: true,
+      showMobamasMenu: true,
+      showMenuMyPage: true,
+      showMenuGacha: false,
+      showMenuCardStr: false,
+      showMenuAuction: false,
+      showMenuQuests: false,
+      showMenuBattles: false,
+      showMenuCardUnion: false,
+      showMenuShop: false,
+      showMenuItem: false,
+      showMenuPresent: false,
+      showMenuCardList: true,
+      showMenuTradeResponse: false,
+      showMenuDeck: false,
+      showMenuExchange: false,
+      showMenuCardStorage: true,
+      showMenuRareParts: false,
+      showMenuFriend: false,
+      showMenuWish: false,
+      showMenuArchive: false,
+      showMenuPRankingAward: true,
+      showMenuResults: false,
+      showMenuGallery: false,
+      showMenuMemory: false,
+      showMenuSBooth: false,
+      showMenuPersonalOption: false,
+      showMenuAdvise: false,
+      showMenuTop: false
+    };
+
+    $scope.save = function() {
+    };
+
+    $scope.cancel = function() {
+    };
+
+    $scope.resetVisited = function() {
+    };
+
+    $scope.resetHiddenDojos = function() {
+    };
+
+    $scope.resetAll = function() {
+    };
+
+    $scope.inputData = function() {
+    };
+
+    $scope.clearDataOutput = function() {
+      $scope.dataOutput = '';
+    };
+  }]);
