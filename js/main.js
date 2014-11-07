2ab969d2cbc0215ba015a32b385b7bdfdaca4142
saasan <saasan.s2works@hotmail.com>
2014-11-07 13:09:26 +0900

    フィルターとか

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 'use strict';
+var dojoMain = angular.module('dojoMain', []);
 
-angular.module('dojoMain', [])
-  .controller('MainController', ['$scope','$http', function($scope, $http) {
-    $scope.$on('$viewContentLoaded', function() {
-      console.log('moge');
-    });
-    $scope.update = function() {
-      $http.get('http://mobamas-dojo-server.herokuapp.com/dojos').
-        success(function(data, status, headers, config) {
-          $scope.message = 'updated!';
-          $scope.dojos = data.dojos;
-        }).
-        error(function(data, status, headers, config) {
-        });
-      console.log('hoge');
-      $scope.message = 'hoge';
-    };
-  }]);
-console.log('fuga');
+dojoMain.controller('MainController', ['$scope','$http', function($scope, $http) {
+  $scope.numberOfShownDojos = 100;
+  $scope.order = '-rank';
+  $scope.rankMin = 0;
+  $scope.rankMax = -1;
+  $scope.levelMin = 0;
+  $scope.levelMax = -1;
+  $scope.defenseMin = 0;
+  $scope.defenseMax = -1;
+
+  $scope.rangeFilter = function(input) {
+    var rank = ($scope.rankMin <= input.rank) && ($scope.rankMax < 0 || input.rank <= $scope.rankMax);
+    var level = ($scope.levelMin <= input.lv) && ($scope.levelMax < 0 || input.lv <= $scope.levelMax);
+    var defense = true;
+
+    // 最小値/最大値が無制限の場合はminDefenseがnullでも表示する
+    if ($scope.defenseMin > 0) {
+      defense = defense && input.minDefense != null && $scope.defenseMin <= input.minDefense;
+    }
+    if ($scope.defenseMax > 0) {
+      defense = defense && input.minDefense != null && input.minDefense <= $scope.defenseMax;
+    }
+
+    return rank && level && defense;
+  };
+
+  $scope.init = function() {
+    $http.get('http://mobamas-dojo-server.herokuapp.com/dojos').
+      success(function(data, status, headers, config) {
+        $scope.message = 'updated!';
+        $scope.dojos = data.dojos;
+      }).
+      error(function(data, status, headers, config) {
+      });
+    console.log('hoge');
+    $scope.message = 'hoge';
+  };
+}]);
