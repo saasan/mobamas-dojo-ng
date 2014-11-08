6d52e94301acea58d4ef291a547bdca50cf0db74
saasan <saasan.s2works@hotmail.com>
2014-11-08 14:54:51 +0900

    viewSettings

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var mobamasDojo = angular.module('mobamasDojo', []);
 
+// リクエストヘッダーにX-Requested-Withを付ける
 mobamasDojo.config(function($httpProvider) {
   $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 });
@@ -7,40 +8,55 @@ mobamasDojo.config(function($httpProvider) {
 mobamasDojo.controller('MainController', ['$scope','$http', function($scope, $http) {
   'use strict';
 
-  $scope.numberOfShownDojos = 30;
-  $scope.order = '-rank';
-  $scope.rankMin = 0;
-  $scope.rankMax = -1;
-  $scope.levelMin = 0;
-  $scope.levelMax = -1;
-  $scope.defenseMin = 0;
-  $scope.defenseMax = -1;
+  // 道場リストの元データ
+  $scope.dojosMaster = {};
 
+  // 表示の設定
+  $scope.viewSettings = {
+    limitTo: 30,
+    orderBy: '-rank',
+    rank: {
+      min: 0,
+      max: -1
+    },
+    level: {
+      min: 0,
+      max: -1
+    },
+    defense: {
+      min: 0,
+      max: -1
+    }
+  };
+
+  // 道場フィルター
   $scope.rangeFilter = function(input) {
-    var rank = ($scope.rankMin <= input.rank) && ($scope.rankMax < 0 || input.rank <= $scope.rankMax);
-    var level = ($scope.levelMin <= input.lv) && ($scope.levelMax < 0 || input.lv <= $scope.levelMax);
+    var rank = ($scope.viewSettings.rank.min <= input.rank) && ($scope.viewSettings.rank.max < 0 || input.rank <= $scope.viewSettings.rank.max);
+    var level = ($scope.viewSettings.level.min <= input.lv) && ($scope.viewSettings.level.max < 0 || input.lv <= $scope.viewSettings.level.max);
     var defense = true;
 
     // 最小値/最大値が無制限の場合はminDefenseがnullでも表示する
-    if ($scope.defenseMin > 0) {
-      defense = defense && input.minDefense != null && $scope.defenseMin <= input.minDefense;
+    if ($scope.viewSettings.defense.min > 0) {
+      defense = defense && input.minDefense != null && $scope.viewSettings.defense.min <= input.minDefense;
     }
-    if ($scope.defenseMax > 0) {
-      defense = defense && input.minDefense != null && input.minDefense <= $scope.defenseMax;
+    if ($scope.viewSettings.defense.max > 0) {
+      defense = defense && input.minDefense != null && input.minDefense <= $scope.viewSettings.defense.max;
     }
 
     return rank && level && defense;
   };
 
+  // 初期化
   $scope.init = function() {
     $http.get('http://mobamas-dojo-server.herokuapp.com/dojos').
       success(function(data, status, headers, config) {
         $scope.message = 'updated!';
-        $scope.dojos = data.dojos;
+        $scope.lastUpdate = data.lastUpdate;
+        $scope.dojosMaster = angular.copy(data.dojos);
+        $scope.dojos = angular.copy(data.dojos);
       }).
       error(function(data, status, headers, config) {
+        $scope.message = 'error!';
       });
-    console.log('hoge');
-    $scope.message = 'hoge';
   };
 }]);
