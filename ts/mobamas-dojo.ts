cd14e031049e3d2f33c834f3f0a5c1c1f2b0d21f
saasan <saasan.s2works@hotmail.com>
2014-11-10 21:56:26 +0900

    jsをtsへ

--- ts/mobamas-dojo.ts
+++ ts/mobamas-dojo.ts
@@ -1,103 +1,94 @@
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
+var mobamasDojo = angular.module('mobamasDojo', ['LocalStorageModule']);
+
+// リクエストヘッダーにX-Requested-Withを付ける
+mobamasDojo.config(function($httpProvider) {
+  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+});
+
+mobamasDojo.config(function(localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('mobamas-dojo');
+});
+
+mobamasDojo.controller('MainController', ['$scope','$http', 'localStorageService', function($scope, $http, localStorageService) {
+  'use strict';
+
+  // ランク表示用文字列
+  $scope.RANK = [ 'F', 'E', 'D', 'C', 'B', 'A', 'S', 'SS', 'S3', 'S4', 'S5' ];
+  // ソート順用データ
+  $scope.ORDER_BY = [
+    { name: 'ランク順', value: ['-rank', '-lv'] },
+    { name: 'レベル順', value: ['-lv', '-rank'] }
+  ];
+
+  // 表示の設定
+  $scope.viewSettings = {
+    limitTo: 30,
+    orderBy: $scope.ORDER_BY[0],
+    rank: {
+      min: 0,
+      max: -1
+    },
+    level: {
+      min: 0,
+      max: -1
+    },
+    defense: {
+      min: 0,
+      max: -1
     }
-    greet() {
-        return "Hello, " + this.greeting;
+  };
+
+  // 道場フィルター
+  $scope.dojoFilter = function(dojo) {
+    // 非表示設定
+    if (dojo.hidden) {
+      return false;
     }
-}
 
-var greeter = new Greeter("world");
+    var rank = ($scope.viewSettings.rank.min <= dojo.rank) && ($scope.viewSettings.rank.max < 0 || dojo.rank <= $scope.viewSettings.rank.max);
+    var level = ($scope.viewSettings.level.min <= dojo.lv) && ($scope.viewSettings.level.max < 0 || dojo.lv <= $scope.viewSettings.level.max);
+    var defense = true;
 
-var button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function() {
-    alert(greeter.greet());
-}
+    // 最小値/最大値が無制限の場合はminDefenseがnullでも表示する
+    if ($scope.viewSettings.defense.min > 0) {
+      defense = defense && dojo.minDefense != null && $scope.viewSettings.defense.min <= dojo.minDefense;
+    }
+    if ($scope.viewSettings.defense.max > 0) {
+      defense = defense && dojo.minDefense != null && dojo.minDefense <= $scope.viewSettings.defense.max;
+    }
 
-document.body.appendChild(button);
+    return rank && level && defense;
+  };
 
+  // 初期化
+  $scope.init = function() {
+    $scope.status = '道場データ読み込み中...';
+    $http.get('http://mobamas-dojo-server.herokuapp.com/dojos').
+      success(function(data, status, headers, config) {
+        // 最終更新日時
+        $scope.lastUpdate = data.lastUpdate;
+        // 道場リスト
+        $scope.dojos = data.dojos;
 
-    _settingsMobamasMenuItems: [
-      { settingsId: 'showMobamasMenu', menuId: 'mobamasMenu' },
-      { settingsId: 'showMenuMyPage', menuId: 'menuMyPage' },
-      { settingsId: 'showMenuGacha', menuId: 'menuGacha' },
-      { settingsId: 'showMenuCardStr', menuId: 'menuCardStr' },
-      { settingsId: 'showMenuAuction', menuId: 'menuAuction' },
-      { settingsId: 'showMenuQuests', menuId: 'menuQuests' },
-      { settingsId: 'showMenuBattles', menuId: 'menuBattles' },
-      { settingsId: 'showMenuCardUnion', menuId: 'menuCardUnion' },
-      { settingsId: 'showMenuShop', menuId: 'menuShop' },
-      { settingsId: 'showMenuItem', menuId: 'menuItem' },
-      { settingsId: 'showMenuPresent', menuId: 'menuPresent' },
-      { settingsId: 'showMenuCardList', menuId: 'menuCardList' },
-      { settingsId: 'showMenuTradeResponse', menuId: 'menuTradeResponse' },
-      { settingsId: 'showMenuDeck', menuId: 'menuDeck' },
-      { settingsId: 'showMenuExchange', menuId: 'menuExchange' },
-      { settingsId: 'showMenuCardStorage', menuId: 'menuCardStorage' },
-      { settingsId: 'showMenuRareParts', menuId: 'menuRareParts' },
-      { settingsId: 'showMenuFriend', menuId: 'menuFriend' },
-      { settingsId: 'showMenuWish', menuId: 'menuWish' },
-      { settingsId: 'showMenuArchive', menuId: 'menuArchive' },
-      { settingsId: 'showMenuPRankingAward', menuId: 'menuPRankingAward' },
-      { settingsId: 'showMenuResults', menuId: 'menuResults' },
-      { settingsId: 'showMenuGallery', menuId: 'menuGallery' },
-      { settingsId: 'showMenuMemory', menuId: 'menuMemory' },
-      { settingsId: 'showMenuSBooth', menuId: 'menuSBooth' },
-      { settingsId: 'showMenuPersonalOption', menuId: 'menuPersonalOption' },
-      { settingsId: 'showMenuAdvise', menuId: 'menuAdvise' },
-      { settingsId: 'showMenuTop', menuId: 'menuTop' }
-    ],
-
-      var defaultValues = {
-        visited: {},
-        hide: {},
-        sameTab: false,
-        visitedMax: 1,
-        autoHide: true,
-        keepLastVisited: true,
-        lastVisited: null,
-        hideBirthday: false,
-        infoClosed: false,
-        lastTime: now
-      };
-
-      // モバマスのメニュー
-      for (var i = 0; i < this._settingsMobamasMenuItems.length; i++) {
-        defaultValues[this._settingsMobamasMenuItems[i].settingsId] = false;
-      }
-      defaultValues.showMobamasMenu = true;       // モバマスのメニュー
-      defaultValues.showMenuMyPage = true;        // ﾏｲｽﾀｼﾞｵ
-      defaultValues.showMenuCardList = true;      // ｱｲﾄﾞﾙ一覧
-      defaultValues.showMenuCardStorage = true;   // 女子寮
-      defaultValues.showMenuPRankingAward = true; // PRA
-
-
-angular.module('dojoApp', [])
-  .controller('TodoController', ['$scope', function($scope) {
-    $scope.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
-
-    $scope.addTodo = function() {
-      $scope.todos.push({text:$scope.todoText, done:false});
-      $scope.todoText = '';
-    };
-
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.todos, function(todo) {
-        count += todo.done ? 0 : 1;
+        $scope.status = '道場データ読み込み完了！';
+      }).
+      error(function(data, status, headers, config) {
+        $scope.status = 'エラー！ ステータスコード: ' + status + ' データ: ' + (data || '(無し)');
       });
-      return count;
-    };
+  };
 
-    $scope.archive = function() {
-      var oldTodos = $scope.todos;
-      $scope.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
-    };
-  }]);
+  // 道場のリンククリック時の処理
+  $scope.onClickDojoLink = function(dojo) {
+    if (dojo.visited) {
+      dojo.visited++;
+    }
+    else {
+      dojo.visited = 1;
+    }
+  };
+
+  // 道場の非表示ボタンクリック時の処理
+  $scope.onClickHideDojo = function(dojo) {
+    dojo.hidden = true;
+  };
+}]);
